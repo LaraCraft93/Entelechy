9ee1a1ce76565bcea8a6f09a0769a57f0e2caf4c
Lara Maia <lara@craft.net.br>
2015-01-18 17:34:10 -0200

    Add a basic environ

---
+++ README.md
@@ -0,0 +1,7 @@
+Entelechy
+=========
+
+This work and all files in this repository is licensed under
+a CC-BY-NC-SA 4.0 International License.
+
+![License](http://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.")
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+declare PROJECTNAME="Entelechy"
+declare INSTALLPREFIX="$HOME/.themes"
+
+test -z "$1" || INSTALLPREFIX="$1"
+
+function create_desktop_entry() {
+cat << EOF > "$1"
+[Desktop Entry]
+Type=X-GNOME-Metatheme
+Name=Entelechy
+Comment=A Perfect beautiful theme
+Encoding=UTF-8
+
+[X-GNOME-Metatheme]
+GtkTheme=Entelechy
+MetacityTheme=Entelechy
+IconTheme=default
+CursorTheme=default
+EOF
+}
+
+install -dm755 "$INSTALLPREFIX/$PROJECTNAME"/{cinnamon/,gtk-2.0/,gtk-3.0/,metacity-1/}/
+install -Dm644 images/cinnamon/* "$INSTALLPREFIX/$PROJECTNAME/cinnamon/"
+install -Dm644 cinnamon.css "$INSTALLPREFIX/$PROJECTNAME/cinnamon/"
+create_desktop_entry "$INSTALLPREFIX/$PROJECTNAME/index.theme"
+
